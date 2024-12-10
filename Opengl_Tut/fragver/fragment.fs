af5fa77122763d502481ff2c5a029809b47265a9
OobayashiRen <devforpalyer@gmail.com>
2024-12-10 16:04:32 +0900

    v0.182 skybox refraction air/glass

--- Opengl_Tut/fragver/fragment.fs
+++ Opengl_Tut/fragver/fragment.fs
@@ -9,9 +9,11 @@ uniform samplerCube skybox;
 
 void main()
 {
+    // light/view 광선이 공기에서 유리로 향하므로 air/glass
+    float ratio = 1.00 / 1.52;
     // view/camera 방향 벡터 i를 계산
     vec3 i = normalize(position - camera_pos);
-    //반사 벡터 r 계산(reflect함수로)
-    vec3 r = reflect(i, normalize(normal));
+    //굴절 벡터 r 계산(refract함수로)
+    vec3 r = refract(i, normalize(normal), ratio);
     frag_color = vec4(texture(skybox, r).rgb, 1.0);
 }
