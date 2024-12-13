4f1c97d9b63e57a893a8fe04f321b7f31018b443
OobayashiRen <devforpalyer@gmail.com>
2024-12-13 14:01:49 +0900

    v0.192 FragDepth(4.2~)

--- Opengl_Tut/fragver/fragment.fs
+++ Opengl_Tut/fragver/fragment.fs
@@ -1,5 +1,6 @@
-#version 330 core
+#version 420 core
 out vec4 frag_color;
+layout (depth_greater) out float gl_FragDepth;
 
 in vec2 tex_coord;
 
@@ -8,10 +9,6 @@ uniform sampler2D back_tex;
 
 void main()
 {
-    if (gl_FrontFacing) {
-        frag_color = texture(front_tex, tex_coord);
-    }
-    else {
-        frag_color = texture(back_tex, tex_coord);
-    }
+    frag_color = vec4(1.0);
+    gl_FragDepth = gl_FragCoord.z + 0.1;
 }
